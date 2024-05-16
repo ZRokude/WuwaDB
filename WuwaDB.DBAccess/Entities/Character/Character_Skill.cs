5399597f2a6abf54c0604afbaac524cd7f276451
Jonathan Axel Wijaya <00324292@rijnijssel.nl>
2024-05-16 09:10:50 +0200

    Database ver 1.0

---
+++ WuwaDB.DBAccess/Entities/Character/Character_Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WuwaDB.Server.Entities.Character
+{
+    public class Character_Skill
+    {
+        public Guid Id { get; set; }
+        public Guid CharacterId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Navigation
+
+        public Character Character { get; set; }
+        public ICollection<Character_Skill_Value> CharacterSkillValues { get; set; }
+    }
+}
---
+++ WuwaDB.DBAccess/Enum/ElementType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WuwaDB.Server.Enum
+{
+    public enum ElementType
+    {
+        Aero,
+        Fusion,
+        Glacio,
+        Havoc,
+        Electro,
+        Spectrum
+    }
+}
