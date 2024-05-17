c7343042ec15e76a625c1240846631184a471c36
ZRoku <zroku89@gmail.com>
2024-05-17 02:05:10 +0200

    Character Card ver 1.0

---
+++ WuwaDB/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+using WuwaDB.Components.Pages;
+
+namespace WuwaDB.Components.Layout
+{
+    public partial class NavMenu
+    {
+        public IDialogService DialogService { get; set; }
+        private void OpenDialog()
+        {
+            var options = new DialogOptions { CloseOnEscapeKey = true };
+            DialogService.Show<Login>();
+        }
+
+    }
+}
